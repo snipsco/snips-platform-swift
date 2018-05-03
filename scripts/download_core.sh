aae45a0b50b82c5c3d9fbde6f01dc862f3482a33
Kevin Lefevre <kevin.lefevre@snips.ai>
2018-05-03 11:00:01 +0200

    Update download_core.sh

--- scripts/download_core.sh
+++ scripts/download_core.sh
@@ -1,99 +1,98 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
  : ${PROJECT_DIR:?"${0##*/} must be invoked as part of an Xcode script phase"}
+
+set -e
 
 VERSION="0.55.2"
 SYSTEM=$(echo $1 | tr '[:upper:]' '[:lower:]')
 LIBRARY_NAME=libsnips_megazord
 LIBRARY_NAME_A=${LIBRARY_NAME}.a
-HEADER_NAME_H=${LIBRARY_NAME}.h
-ROOT_DIR=${PROJECT_DIR}/../../../
+LIBRARY_NAME_H=${LIBRARY_NAME}.h
+OUT_DIR=${PROJECT_DIR}/Dependencies/${SYSTEM}
 
-if [ -z ${TARGET_BUILD_TYPE} ]; then
+if [ -z "$TARGET_BUILD_TYPE" ]; then
 TARGET_BUILD_TYPE=$(echo ${CONFIGURATION} | tr '[:upper:]' '[:lower:]')
 fi
 
-if [ ${SYSTEM} != ios ] && [ ${SYSTEM} != macos ]; then
-    echo "Given system should be 'ios' or 'macos'."
+if [ "${SYSTEM}" != "ios" ]; then
+    echo "Only ios is supported."
     exit 1
 fi
 
-OUTDIR=${PROJECT_DIR}/Dependencies/${SYSTEM}
-mkdir -p ${OUTDIR}
+mkdir -p ${OUT_DIR}
 
-install_remote_core_platform () {
+install_remote_core () {
     local filename=snips-platform-${SYSTEM}.${VERSION}.tgz
     local url=https://s3.amazonaws.com/snips/snips-platform-dev/${filename}
 
-    rm -f ${OUTDIR}/*
-    if ! core_platform_is_present; then
+    rm -f ${OUT_DIR}/*
+    if ! core_is_present; then
         echo "Will download '${filename}'"
-        cd ${PROJECT_DIR}/Dependencies/${SYSTEM}
-        curl -s ${url} | tar zxv
+        $(cd ${OUT_DIR} && curl -s ${url} | tar zxv)
     fi
 }
 
-install_local_core_platform () {
-    if [ ! -e ${ROOT_DIR}/Makefile ]; then
+install_local_core () {
+    # TODO: Find a better way to retrieve root_dir
+    local root_dir=${PROJECT_DIR}/../../../
+    local target_dir=${root_dir}/target/
+
+    if [ ! -e ${root_dir}/Makefile ]; then
         return 1
     fi
-    echo "Attempt to use iOS local build"
 
-    make -C ${ROOT_DIR} package-megazord-dependencies-ios
+    make -C ${root_dir} package-megazord-dependencies-ios
 
-    rm -f ${OUTDIR}/*
-    for arch in "${ARCHS_ARRAY[@]}"; do
-        if [ ${arch} = arm64 ]; then
-            local arch=aarch64
-        fi
-        local library_path=${ROOT_DIR}/target/${arch}-apple-ios/${TARGET_BUILD_TYPE}/${LIBRARY_NAME_A}
-        if [ ! -e ${library_path} ]; then
-            return 1
-        fi
-        cp ${library_path} ${OUTDIR}/${LIBRARY_NAME}-${arch}.a
-    done
+    rm -f ${OUT_DIR}/*
 
-    lipo -create `find ${OUTDIR}/${LIBRARY_NAME}-*.a` -output ${OUTDIR}/${LIBRARY_NAME_A}
-    cp ${ROOT_DIR}/target/ios-universal/megazord/* ${OUTDIR}
+    if [ ${SYSTEM} == ios ]; then
+        echo "Using iOS local build"
+        local archs_array=( ${ARCHS} )
+
+        for arch in "${archs_array[@]}"; do
+            if [ ${arch} = arm64 ]; then
+                local arch=aarch64
+            fi
+            local library_path=${target_dir}/${arch}-apple-ios/${TARGET_BUILD_TYPE}/${LIBRARY_NAME_A}
+            if [ ! -e ${library_path} ]; then
+                return 1
+            fi
+            cp ${library_path} ${OUT_DIR}/${LIBRARY_NAME}-${arch}.a
+        done
+
+        lipo -create `find ${OUT_DIR}/${LIBRARY_NAME}-*.a` -output ${OUT_DIR}/${LIBRARY_NAME_A}
+        cp ${target_dir}/ios-universal/megazord/* ${OUT_DIR}
+
+    else
+        echo "${SYSTEM} isn't supported"
+        return 1
+    fi
 
     return 0
 }
 
-core_platform_is_present () {
-    if [ -e ${PROJECT_DIR}/Dependencies/${SYSTEM}/libprotobuf.a ] &&
-       [ -e ${PROJECT_DIR}/Dependencies/${SYSTEM}/libtensorflow.a ] &&
-       [ -e ${PROJECT_DIR}/Dependencies/${SYSTEM}/module.modulemap ] &&
-       [ -e ${PROJECT_DIR}/Dependencies/${SYSTEM}/${LIBRARY_NAME_A} ] &&
-       [ -e ${PROJECT_DIR}/Dependencies/${SYSTEM}/${HEADER_NAME_H} ]; then
+core_is_present () {
+    if [ -e ${OUT_DIR}/libprotobuf.a ] &&
+       [ -e ${OUT_DIR}/libtensorflow.a ] &&
+       [ -e ${OUT_DIR}/module.modulemap ] &&
+       [ -e ${OUT_DIR}/${LIBRARY_NAME_A} ] &&
+       [ -e ${OUT_DIR}/${LIBRARY_NAME_H} ]; then
         return 0
     fi
 
     return 1
 }
 
-case ${SYSTEM} in
-    macos)
-        echo "The platform macOS isn't yet supported."
-        exit 1
-    ;;
-    ios)
-        ARCHS_ARRAY=( ${ARCHS} )
+if [ "${SNIPS_USE_LOCAL_PLATFORM}" == 1 ]; then
+    install_local_core && exit 0
+else
+    if core_is_present; then
+        # TODO: Should check for version
+        exit 0
+    fi
 
-        if [ "${SNIPS_USE_LOCAL_PLATFORM}" == 1 ]; then
-            install_local_core_platform && exit 0
-        else
-            if core_platform_is_present; then
-                # TODO: Should check for version
-                exit 0
-            fi
-
-            if ! install_local_core_platform; then
-                install_remote_core_platform && exit 0
-            fi
-        fi
-    ;;
-    *)
-        echo "This platform isn't supported."
-        exit 1
-    ;;
-esac
+    if ! install_local_core; then
+        install_remote_core && exit 0
+    fi
+fi
