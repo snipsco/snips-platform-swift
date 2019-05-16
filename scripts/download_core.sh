e6263604079a379f6b377a0d9e389dfa615c5f33
Kevin Lefèvre <kevin.lefevre@snips.ai>
2019-05-16 14:59:21 +0200

    Fix issue when path contains a space

--- scripts/download_core.sh
+++ scripts/download_core.sh
@@ -10,7 +10,7 @@ LIBRARY_NAME_H=${LIBRARY_NAME}.h
 
 OUT_DIR=${PROJECT_DIR}/Dependencies/
 
-install_remote_core () {
+install_remote_core() {
     echo "Trying remote installation"
 
     local filename=snips-platform-cocoa.${VERSION}.tgz
@@ -18,7 +18,7 @@ install_remote_core () {
 
     echo "Will download and decompress '${filename}' into '${OUT_DIR}'"
     if curl --output /dev/null --silent --head --fail "$url"; then
-        $(cd ${OUT_DIR} && curl -s ${url} | tar zxv)
+        $(cd "${OUT_DIR}" && curl -s ${url} | tar zxv)
     else
         echo "Version ${VERSION} doesn't seem to have been released yet" >&2
         echo "Could not find any file at '${url}'" >&2
@@ -27,22 +27,22 @@ install_remote_core () {
     fi
 }
 
-core_is_present () {
+core_is_present() {
     echo "Checking if core is present and complete"
 
     local files_to_check=(
-        ${OUT_DIR}/${LIBRARY_NAME_H}
-        ${OUT_DIR}/module.modulemap
+        "${OUT_DIR}/${LIBRARY_NAME_H}"
+        "${OUT_DIR}/module.modulemap"
     )
-    if [ -z ${SNIPS_TARGET_PLATFORM} ] || [ ${SNIPS_TARGET_PLATFORM} = "ios" ]; then
-        files_to_check+=(${OUT_DIR}/${LIBRARY_NAME_A_IOS})
+    if [[ -z ${SNIPS_TARGET_PLATFORM} ]] || [[ ${SNIPS_TARGET_PLATFORM} = "ios" ]]; then
+        files_to_check+=("${OUT_DIR}/${LIBRARY_NAME_A_IOS}")
     fi
-    if [ -z ${SNIPS_TARGET_PLATFORM} ] || [ ${SNIPS_TARGET_PLATFORM} = "macos" ]; then
-        files_to_check+=(${OUT_DIR}/${LIBRARY_NAME_A_MACOS})
+    if [ -z ${SNIPS_TARGET_PLATFORM} ] || [[ ${SNIPS_TARGET_PLATFORM} = "macos" ]]; then
+        files_to_check+=("${OUT_DIR}/${LIBRARY_NAME_A_MACOS}")
     fi
     
     for file in "${files_to_check[@]}"; do
-        if [ ! -e $file ]; then
+        if [ ! -e "${file}" ]; then
             echo "Core isn't complete" >&2
             echo "Missing file '$file'" >&2
             return 1
@@ -53,13 +53,13 @@ core_is_present () {
     return 0
 }
 
-core_is_up_to_date () {
+core_is_up_to_date() {
     echo "Checking if core is up-to-date"
 
     local header_path=${OUT_DIR}/${LIBRARY_NAME_H}
-    local core_version=$(grep "SNIPS_VERSION" ${header_path} | cut -d'"' -f2)
+    local core_version=$(grep "SNIPS_VERSION" "${header_path}" | cut -d'"' -f2)
 
-    if [ "$core_version" = ${VERSION} ]; then
+    if [[ "$core_version" = "${VERSION}" ]]; then
         echo "Core is up-to-date"
         return 0
     fi
@@ -69,12 +69,12 @@ core_is_up_to_date () {
 }
 
 main() {
-    if [ -n "${SNIPS_USE_LOCAL}" ]; then
+    if [[ -n "${SNIPS_USE_LOCAL}" ]]; then
         echo "SNIPS_USE_LOCAL is set. Will try local installation only"
         return 0
     fi
     
-    if [ -n "${SNIPS_FORCE_REINSTALL}" ]; then
+    if [[ -n "${SNIPS_FORCE_REINSTALL}" ]]; then
         echo "SNIPS_FORCE_REINSTALL is set. Skipping checks and reinstalling core"
     else
         if core_is_present && core_is_up_to_date; then
@@ -83,9 +83,9 @@ main() {
         fi
     fi
 
-    mkdir -p ${OUT_DIR}
+    mkdir -p "${OUT_DIR}"
     echo "Cleaning '${OUT_DIR}' content"
-    rm -f ${OUT_DIR}/*
+    rm -f "${OUT_DIR}/*"
 
     install_remote_core && return 0
 }
