5ae0635027235d68ff99e9479af727f0af553411
anthonyray <dantho361@hotmail.com>
2019-07-29 16:54:05 +0200

    [RELEASE] 1.3.0 0.64.0-beta3

--- scripts/download_core.sh
+++ scripts/download_core.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="0.64.0-SNAPSHOT"
+VERSION="0.64.0-beta3"
 LIBRARY_NAME=libsnips_megazord
 LIBRARY_NAME_A_IOS=${LIBRARY_NAME}-ios.a
 LIBRARY_NAME_A_MACOS=${LIBRARY_NAME}-macos.a
